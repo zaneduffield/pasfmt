672c14b6426cbb89b6b5caf76d0f1126db46c9ce
Joshua Gardner <jgardner@integradev.com.au>
2023-06-27 08:24:51 +1000

    Add formatter to remove repeated blank lines

--- pasfmt-core/src/rules/mod.rs
+++ pasfmt-core/src/rules/mod.rs
@@ -1,2 +1,3 @@
+pub mod remove_repeated_newlines;
 pub mod uses_clause_consolidator;
 pub mod uses_clause_formatter;
---
+++ pasfmt-core/src/rules/remove_repeated_newlines.rs
@@ -0,0 +1,73 @@
+use std::cmp::min;
+
+use crate::{lang::*, traits::LogicalLineFormatter};
+
+pub struct RemoveRepeatedNewlines {}
+impl LogicalLineFormatter for RemoveRepeatedNewlines {
+    fn format<'a>(
+        &self,
+        mut formatted_tokens: FormattedTokens<'a>,
+        input: &LogicalLine,
+    ) -> FormattedTokens<'a> {
+        for &token_index in input.get_tokens() {
+            let token_formatting_data =
+                formatted_tokens.get_or_create_formatting_data_mut(token_index);
+            *token_formatting_data.get_newlines_before_mut() =
+                min(token_formatting_data.get_newlines_before(), 2);
+        }
+
+        formatted_tokens
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use spectral::prelude::*;
+
+    use super::*;
+    use crate::{
+        defaults::lexer::DelphiLexer, defaults::parser::DelphiLogicalLineParser,
+        defaults::reconstructor::DelphiLogicalLinesReconstructor, formatter::Formatter,
+    };
+
+    fn run_test(input: &'static str, output: &'static str) {
+        let formatter = Formatter::new(
+            Box::new(DelphiLexer {}),
+            vec![],
+            Box::new(DelphiLogicalLineParser {}),
+            vec![],
+            vec![Box::new(RemoveRepeatedNewlines {})],
+            Box::new(DelphiLogicalLinesReconstructor::new(
+                ReconstructionSettings::new("\n".to_string(), "  ".to_string(), "  ".to_string()),
+            )),
+        );
+
+        let formatted_output = formatter.format(input);
+        assert_that(&formatted_output).is_equal_to(output.to_string());
+    }
+
+    #[test]
+    fn no_new_lines_before() {
+        run_test("Foo; Bar;", "Foo; Bar;");
+    }
+
+    #[test]
+    fn one_new_line_before() {
+        run_test("Foo;\nBar;", "Foo;\nBar;");
+    }
+
+    #[test]
+    fn two_new_lines_before() {
+        run_test("Foo;\n\nBar;", "Foo;\n\nBar;");
+    }
+
+    #[test]
+    fn three_new_lines_before() {
+        run_test("Foo;\n\n\nBar;", "Foo;\n\nBar;");
+    }
+
+    #[test]
+    fn four_new_lines_before() {
+        run_test("Foo;\n\n\n\nBar;", "Foo;\n\nBar;");
+    }
+}
