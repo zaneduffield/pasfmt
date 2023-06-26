997c85826d8926e8b3936b975ecdb8c9d6c89e07
Joshua Gardner <jgardner@integradev.com.au>
2023-06-26 16:03:11 +1000

    Add formatter selector

---
+++ pasfmt-core/src/formatter_selector.rs
@@ -0,0 +1,115 @@
+use crate::lang::*;
+use crate::traits::*;
+
+#[allow(dead_code)]
+pub struct FormatterSelector<'a, T>
+where
+    T: Fn(LogicalLineType) -> Option<&'a dyn LogicalLineFormatter>,
+{
+    selector: T,
+}
+#[allow(dead_code)]
+impl<'a, T> FormatterSelector<'a, T>
+where
+    T: Fn(LogicalLineType) -> Option<&'a dyn LogicalLineFormatter>,
+{
+    pub fn new(selector: T) -> Self {
+        FormatterSelector { selector }
+    }
+}
+#[allow(dead_code)]
+impl<'a, T> LogicalLineFormatter for FormatterSelector<'a, T>
+where
+    T: Fn(LogicalLineType) -> Option<&'a dyn LogicalLineFormatter>,
+{
+    fn format<'b>(
+        &self,
+        formatted_tokens: FormattedTokens<'b>,
+        input: &LogicalLine,
+    ) -> FormattedTokens<'b> {
+        if let Some(formatter) = (self.selector)(input.get_line_type()) {
+            formatter.format(formatted_tokens, input)
+        } else {
+            formatted_tokens
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::{
+        defaults::{
+            lexer::DelphiLexer, parser::DelphiLogicalLineParser,
+            reconstructor::DelphiLogicalLinesReconstructor,
+        },
+        formatter::Formatter,
+    };
+
+    use super::*;
+    use spectral::prelude::*;
+
+    fn run_test(formatter: Formatter, input: &str, expected_output: &str) {
+        let output = formatter.format(input);
+        assert_that(&output).is_equal_to(expected_output.to_string());
+    }
+
+    struct Add1Indentation;
+    impl LogicalLineFormatter for Add1Indentation {
+        fn format<'a>(
+            &self,
+            formatted_tokens: FormattedTokens<'a>,
+            input: &LogicalLine,
+        ) -> FormattedTokens<'a> {
+            let (tokens, mut formatting_data) = formatted_tokens.own_data();
+            let first_token = *input.get_tokens().first().unwrap();
+            formatting_data.push(FormattingData::new(first_token));
+            *formatting_data
+                .last_mut()
+                .unwrap()
+                .get_indentations_before_mut() += 1;
+            FormattedTokens::new(tokens, formatting_data)
+        }
+    }
+
+    struct Add1Continuation;
+    impl LogicalLineFormatter for Add1Continuation {
+        fn format<'a>(
+            &self,
+            formatted_tokens: FormattedTokens<'a>,
+            input: &LogicalLine,
+        ) -> FormattedTokens<'a> {
+            let (tokens, mut formatting_data) = formatted_tokens.own_data();
+            let first_token = *input.get_tokens().first().unwrap();
+            formatting_data.push(FormattingData::new(first_token));
+            *formatting_data
+                .last_mut()
+                .unwrap()
+                .get_continuations_before_mut() += 1;
+            FormattedTokens::new(tokens, formatting_data)
+        }
+    }
+
+    #[test]
+    fn optional_formatter_selector() {
+        let add_1_indentation = &Add1Indentation {};
+        let add_1_continuation = &Add1Continuation {};
+        let formatter = Formatter::new(
+            Box::new(DelphiLexer {}),
+            vec![],
+            Box::new(DelphiLogicalLineParser {}),
+            vec![],
+            vec![Box::new(FormatterSelector {
+                selector: |line_type| match line_type {
+                    LogicalLineType::Unknown => Some(add_1_indentation),
+                    LogicalLineType::Eof => Some(add_1_continuation),
+                    _ => None,
+                },
+            })],
+            Box::new(DelphiLogicalLinesReconstructor::new(
+                ReconstructionSettings::new("\n".to_owned(), " i".to_owned(), " c".to_owned()),
+            )),
+        );
+        run_test(formatter, "a;", " ia; c");
+    }
+}
--- pasfmt-core/src/lib.rs
+++ pasfmt-core/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod defaults;
 pub mod formatter;
+pub mod formatter_selector;
 pub mod lang;
 pub mod rules;
 pub mod traits;
