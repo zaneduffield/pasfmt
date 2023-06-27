184df9b09f02f26fcadaff8a3ae5e220debee184
Joshua Gardner <jgardner@integradev.com.au>
2023-06-27 14:02:27 +1000

    Add pasfmt-orchestrator to pasfmt-front-end

---
+++ pasfmt-orchestrator/src/formatting_orchestrator.rs
@@ -0,0 +1,29 @@
+use std::io::Read;
+
+use pasfmt_core::formatter::Formatter;
+
+use crate::{command_line::PasFmtConfiguration, file_formatter::FileFormatter};
+
+pub struct FormattingOrchestrator;
+impl FormattingOrchestrator {
+    pub fn run(formatter: Formatter) {
+        let config = PasFmtConfiguration::new();
+        let files = config.get_paths();
+        let file_refs: Vec<_> = files.iter().map(|string| string.as_str()).collect();
+        let file_formatter = FileFormatter::new(formatter);
+
+        if file_refs.is_empty() {
+            let mut input = String::new();
+            let mut stdin = std::io::stdin().lock();
+
+            stdin.read_to_string(&mut input).unwrap();
+            file_formatter.format_stdin_to_stdout(&input);
+        } else if config.is_verify() {
+            file_formatter.check_files(file_refs);
+        } else if config.is_write() {
+            file_formatter.format_files(file_refs);
+        } else {
+            file_formatter.format_files_to_stdout(file_refs);
+        }
+    }
+}
--- pasfmt-orchestrator/src/lib.rs
+++ pasfmt-orchestrator/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod command_line;
 pub mod file_formatter;
+pub mod formatting_orchestrator;
