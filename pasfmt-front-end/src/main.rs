873197a8ecd17bbf5c9555c411b60ba548fa3764
Joshua Gardner <jgardner@integradev.com.au>
2023-06-30 10:13:05 +1000

    Specify win1252 as encoding in main

--- pasfmt-front-end/src/main.rs
+++ pasfmt-front-end/src/main.rs
@@ -14,7 +14,8 @@ use pasfmt_core::{
     },
 };
 use pasfmt_orchestrator::{
-    command_line::PasFmtConfiguration, formatting_orchestrator::FormattingOrchestrator,
+    command_line::PasFmtConfiguration, file_formatter::FileFormatter,
+    formatting_orchestrator::FormattingOrchestrator,
 };
 use serde_derive::Deserialize;
 
@@ -44,31 +45,34 @@ fn main() {
     let formatting_settings = config.get_config_object::<FormattingSettings>();
 
     let uses_clause_formatter = &UsesClauseFormatter {};
-    let formatter = Formatter::new(
-        Box::new(DelphiLexer {}),
-        vec![],
-        Box::new(DelphiLogicalLineParser {}),
-        vec![Box::new(UsesClauseConsolidator {})],
-        vec![
-            Box::new(RemoveTrailingWhitespace {}),
-            Box::new(RemoveRepeatedNewlines {}),
-            Box::new(FormatterSelector::new(
-                |logical_line_type| match logical_line_type {
-                    LogicalLineType::UsesClause => Some(uses_clause_formatter),
-                    _ => None,
-                },
+    let formatter = FileFormatter::new(
+        Formatter::new(
+            Box::new(DelphiLexer {}),
+            vec![],
+            Box::new(DelphiLogicalLineParser {}),
+            vec![Box::new(UsesClauseConsolidator {})],
+            vec![
+                Box::new(RemoveTrailingWhitespace {}),
+                Box::new(RemoveRepeatedNewlines {}),
+                Box::new(FormatterSelector::new(
+                    |logical_line_type| match logical_line_type {
+                        LogicalLineType::UsesClause => Some(uses_clause_formatter),
+                        _ => None,
+                    },
+                )),
+            ],
+            Box::new(DelphiLogicalLinesReconstructor::new(
+                ReconstructionSettings::new(
+                    formatting_settings.reconstruction.eol,
+                    formatting_settings.reconstruction.indentation.clone(),
+                    formatting_settings
+                        .reconstruction
+                        .continuation
+                        .unwrap_or(formatting_settings.reconstruction.indentation),
+                ),
             )),
-        ],
-        Box::new(DelphiLogicalLinesReconstructor::new(
-            ReconstructionSettings::new(
-                formatting_settings.reconstruction.eol,
-                formatting_settings.reconstruction.indentation.clone(),
-                formatting_settings
-                    .reconstruction
-                    .continuation
-                    .unwrap_or(formatting_settings.reconstruction.indentation),
-            ),
-        )),
+        ),
+        encoding_rs::WINDOWS_1252,
     );
     FormattingOrchestrator::run(formatter);
 }
